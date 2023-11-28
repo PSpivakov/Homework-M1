e99231820ab8107a4b1da8595cbc264805514b97
Pablo <pavlospivakov@gmail.com>
2023-11-28 15:32:16 +0100

    Task-2 Делимость на 3

--- Homework-M1/main.swift
+++ Homework-M1/main.swift
@@ -9,3 +9,13 @@ func taskOne() -> Bool {
 }
 
 taskOne() == true ? print("Число четное") : print("Число нечетное")
+
+
+// Задание 2: Делимость на 3
+
+var number2 = 27
+func taskTwo() -> Bool {
+    number2 % 3 == 0 ? true : false
+}
+
+taskTwo() == true ? print("Число делится на 3 без остатка") : print("Число не делится на 3 без остатка")
