c99aa87ef582bca6eb1f754fe847077e0e3f478c
Pablo <pavlospivakov@gmail.com>
2023-11-28 15:06:08 +0100

    Task-1 Проверка булевых значений

--- Homework-M1/main.swift
+++ Homework-M1/main.swift
@@ -1,11 +1,18 @@
-//
-//  main.swift
-//  Homework-M1
-//
-//  Created by Pavlo Spivakov on 28.11.23.
-//
-
 import Foundation
 
 print("Hello, World!")
+// 1. Задание: Проверка булевых условий
+let day = false
+let night = true
 
+
+if day == night {
+    print("Оба условия выполняются")
+} else {
+    print("Не все условия выполняются")
+    if day != night {
+        print("Хотя бы одно из условий выполняется")
+    } else {
+        print("Оба условия ложны")
+    }
+}
