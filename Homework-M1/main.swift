3113310e5895797a1619b46d2f3d03e91823122a
Pablo <pavlospivakov@gmail.com>
2023-12-02 15:04:22 +0100

    Task-1 Сортировка чисел

--- Homework-M1/main.swift
+++ Homework-M1/main.swift
@@ -1,11 +1,25 @@
-//
-//  main.swift
-//  Homework-M1
-//
-//  Created by Pavlo Spivakov on 28.11.23.
-//
-
 import Foundation
 
-print("Hello, World!")
+// Задание 1: Сортировка чисел
 
+
+func sortNum(array: [Int]) -> [Int] {
+    var resultNumArray1 = array
+    let n = resultNumArray1.count
+    
+    for i in 0 ..< n - 1 {
+        for x in 0 ..< n - i  - 1 {
+            if resultNumArray1[x] > resultNumArray1[x + 1] {
+                let temp = resultNumArray1[x]
+                resultNumArray1[x] = resultNumArray1[x + 1]
+                resultNumArray1[x + 1] = temp
+            }
+        }
+    }
+    return resultNumArray1
+}
+
+
+var numArray1 = [29, 81, 8, 35, 74, 1, 0, 78, 99, 23]
+let resultSort1 = sortNum(array: numArray1)
+print("Отсортированный массив с числами \(resultSort1)")
